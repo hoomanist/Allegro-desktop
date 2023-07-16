7ce49b302d5ec61f8fc106613c1f12d678402c54
Hooman Hedayati <its.hoomanist@gmail.com>
2023-07-16 12:08:01 +0330

    switched from progress bar to an slider

--- pkg/audio/play.go
+++ pkg/audio/play.go
@@ -4,11 +4,18 @@ import (
 	"os"
 	"time"
 
+	"github.com/faiface/beep"
 	"github.com/faiface/beep/flac"
 	"github.com/faiface/beep/speaker"
 )
 
-func PlayMusic(path string) {
+type AudioPanel struct {
+	Ctrl     *beep.Ctrl
+	Format   beep.Format
+	Streamer beep.StreamSeekCloser
+}
+
+func MakeStreamer(path string) *AudioPanel {
 	f, err := os.Open(path)
 	if err != nil {
 		panic("no such file")
@@ -17,8 +24,11 @@ func PlayMusic(path string) {
 	if err != nil {
 		panic("can't decode")
 	}
+	ctrl := &beep.Ctrl{Streamer: streamer}
+	return &AudioPanel{Ctrl: ctrl, Streamer: streamer, Format: format}
+}
+func PlayMusic(streamer beep.StreamCloser, format beep.Format) {
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 	speaker.Play(streamer)
 	select {}
-	defer streamer.Close()
 }
--- pkg/gui/window.go
+++ pkg/gui/window.go
@@ -1,10 +1,16 @@
 package gui
 
 import (
+	"fmt"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/faiface/beep/speaker"
 	"github.com/hoomanist/Allegro/pkg/audio"
 )
 
@@ -12,9 +18,31 @@ func MainWindow() {
 	a := app.New()
 	w := a.NewWindow("Allegro")
 	w.Resize(fyne.NewSize(900, 600))
-	play_button := widget.NewButtonWithIcon("play", theme.MediaPlayIcon(), func() {
-		audio.PlayMusic("/home/hooman/test.flac")
+	ap := audio.MakeStreamer("/home/hooman/test.flac")
+	go audio.PlayMusic(ap.Streamer, ap.Format)
+	speaker.Lock()
+	ap.Ctrl.Paused = true
+	speaker.Unlock()
+	play_button := widget.NewButtonWithIcon("", theme.MediaPlayIcon(), func() {
+		speaker.Lock()
+		ap.Ctrl.Paused = !ap.Ctrl.Paused
+		fmt.Println(ap.Ctrl.Paused)
+		speaker.Unlock()
 	})
-	w.SetContent(play_button)
+	slider := widget.NewSlider(0, float64(ap.Streamer.Len()))
+
+	go func() {
+		for ap.Streamer.Position() != ap.Streamer.Len() {
+			if !ap.Ctrl.Paused {
+				play_button.SetIcon(theme.MediaPauseIcon())
+			} else {
+				play_button.SetIcon(theme.MediaPlayIcon())
+			}
+			slider.SetValue(float64(ap.Streamer.Position()))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	content := container.New(layout.NewVBoxLayout(), container.New(layout.NewCenterLayout(), play_button), slider)
+	w.SetContent(content)
 	w.ShowAndRun()
 }
